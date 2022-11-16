a3d9dbb88409c698625ad98674a9f85e4542aceb
Svetlana Tambolskay <svetlanatambolskay@gmail.com>
2022-11-16 18:43:45 +0400

    Work with text fields

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -15,14 +15,18 @@ public class MainClass {
         driver.manage().timeouts().implicitlyWait (10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.get("http://en.wikipedia.org");
+       /* driver.get("http://en.wikipedia.org");
+        driver.findElement(By.xpath("//input[@ id='searchInput']")).sendKeys("Selenium WebDriver");
         driver.findElement(By.xpath("//input[@id ='searchButton']")).click();
+        System.out.println(driver.findElement(By.xpath("//div[@id='searchText']/input")).getAttribute("value"));
+        driver.findElement(By.xpath("//div[@id='searchText']/input")).clear();
+*/
+       driver.get("http://github.com");
+       driver.findElement(By.xpath("//a[@href='/login']")).click();
+       driver.findElement(By.xpath("//form[@data-turbo='false']/input[@id='login_field']")).sendKeys("Password");
+       WebElement button = driver.findElement(By.xpath("//input[@class='btn btn-primary btn-block js-sign-in-button']"));
 
-        driver.get("http://github.com");
-        driver.findElement(By.xpath("//a[@href='/login']")).click();
-        WebElement button = driver.findElement(By.xpath("//input[@class='btn btn-primary btn-block js-sign-in-button']"));
 
-        System.out.println("Button text is: " + button.getAttribute("value"));
 
         button.submit();
     }
